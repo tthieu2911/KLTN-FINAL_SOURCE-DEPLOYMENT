abf97478db7ab0455ac54c2b03cf80ab6800ea41
tthieu2911@gmail.com <1512162@student.hcmus.edu.vn>
2019-05-29 01:47:22 +0800

    testing DB

--- data/config.js
+++ data/config.js
@@ -1,5 +1,7 @@
 module.exports = {
 
     //'url' : 'mongodb://admin:admin123@ds219098.mlab.com:19098/kltn_test01' 
-    'url' : 'mongodb://localhost:27017/myDB_Block' 
+    'url' : 'mongodb://admin:admin123@ds263146.mlab.com:63146/kltn_test02' 
+    
+    //'url' : 'mongodb://localhost:27017/myDB_Block' 
 };
